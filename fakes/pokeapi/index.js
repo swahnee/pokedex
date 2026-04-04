beb27f3ce5deb940e4f5891f69393e27c83a20cc
swahnee <swahnee.july@gmail.com>
2026-04-04 17:49:29 +0200

    Add main entrypoint with fake data (#2)
    Co-authored-by: Michele Zamuner <michelezamuner@gmail.com>

---
+++ fakes/pokeapi/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const mewtwo = require("./fixtures/mewtwo.json");
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/api/v2/pokemon-species/:name", (req, res) => {
+  if (req.params.name === "mewtwo") {
+    return res.json(mewtwo);
+  }
+
+  return res.status(400).send("Bad Request");
+});
+
+app.listen(4000, () => {
+  console.log("App started on port 4000");
+});
