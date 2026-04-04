76ecb9ab092c21f1e1ee0e82b5e2c29816d77895
Michele Zamuner <michelezamuner@gmail.com>
2026-04-04 17:41:31 +0200

    Ignore errors for now

--- src/app.js
+++ src/app.js
@@ -9,22 +9,16 @@ const pokeapiUrl = `${process.env.POKEAPI_HOST}:${process.env.POKEAPI_PORT}`;
 
 app.get("/pokemon/:name", async (req, res) => {
   const name = req.params.name;
-  try {
-    const response = await fetch(
-      `${pokeapiUrl}/api/v2/pokemon-species/${name}`
-    );
-    const data = await response.json();
-    res.json({
-      name: name,
-      description: data.flavor_text_entries[0].flavor_text,
-      habitat: data.habitat.name,
-      isLegendary: data.is_legendary,
-    });
-  } catch (e) {
-    res.json({
-      error: "Error getting resource",
-    });
-  }
+  const response = await fetch(
+    `${pokeapiUrl}/api/v2/pokemon-species/${name}`
+  );
+  const data = await response.json();
+  res.json({
+    name: name,
+    description: data.flavor_text_entries[0].flavor_text,
+    habitat: data.habitat.name,
+    isLegendary: data.is_legendary,
+  });
 });
 
 module.exports = app;
