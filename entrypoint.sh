cb2dbef22aa249bb4182d183d03187a768728fd7
Swahnee <swahnee.july@gmail.com>
2026-04-06 08:10:13 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ if [ "$APP_ENV" = "prod" ]; then
   echo "Starting production server..."
   exec node index.js
 elif [ "$APP_ENV" = "ci" ]; then
+  trap 'exit 0' SIGTERM
   echo "Running CI..."
   exec npm run ci
 else
