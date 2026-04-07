24a9bda7d1d98f71d6f7344f3e5ed084ac42554f
Swahnee <swahnee.july@gmail.com>
2026-04-07 19:35:15 +0200

    Improvements

--- fakes/funtranslations/index.js
+++ fakes/funtranslations/index.js
@@ -17,19 +17,20 @@ app.post("/v1/translate/yoda", (req, res) => {
     });
   }
 
-  if (req.body.text === "error") {
-    return res.status(500).send({
-      error: {
-        code: 500,
-        message: "Internal Server Error",
+  if (req.body.text.startsWith("It was created")) {
+    return res.json({
+      success: {
+        total: 1,
+      },
+      contents: {
+        translated: "Yoda translated Mewtwo description",
+        text: "Original Mewtwo description",
+        translation: "yoda",
       },
     });
   }
 
-  if (
-    req.body.text ===
-    "Forms colonies in\nperpetually dark\nplaces. Uses\fultrasonic waves\nto identify and\napproach targets."
-  ) {
+  if (req.body.text.startsWith("Forms colonies")) {
     return res.json({
       success: {
         total: 1,
@@ -42,14 +43,10 @@ app.post("/v1/translate/yoda", (req, res) => {
     });
   }
 
-  return res.json({
-    success: {
-      total: 1,
-    },
-    contents: {
-      translated: "Yoda translated Mewtwo description",
-      text: "Original Mewtwo description",
-      translation: "yoda",
+  return res.status(500).send({
+    error: {
+      code: 500,
+      message: "Internal Server Error",
     },
   });
 });
@@ -64,23 +61,23 @@ app.post("/v1/translate/shakespeare", (req, res) => {
     });
   }
 
-  if (req.body.text === "error") {
-    return res.status(500).send({
-      error: {
-        code: 500,
-        message: "Internal Server Error",
+  if (req.body.text.startsWith("When it folds")) {
+    return res.json({
+      success: {
+        total: 1,
+      },
+      contents: {
+        translated: "Shakespeare translated Garchomp description",
+        text: "Original Garchomp description",
+        translation: "shakespeare",
       },
     });
   }
 
-  return res.json({
-    success: {
-      total: 1,
-    },
-    contents: {
-      translated: "Shakespeare translated Garchomp description",
-      text: "Original Garchomp description",
-      translation: "shakespeare",
+  return res.status(500).send({
+    error: {
+      code: 500,
+      message: "Internal Server Error",
     },
   });
 });
--- tests/drivers/translations-service/funtranslations.test.js
+++ tests/drivers/translations-service/funtranslations.test.js
@@ -5,7 +5,7 @@ describe("funtranslations", () => {
   it("translates given text with yoda translation", async () => {
     const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
 
-    const translation = await service.translateYoda("valid text");
+    const translation = await service.translateYoda("It was created");
 
     expect(translation).toBe("Yoda translated Mewtwo description");
   });
@@ -13,12 +13,12 @@ describe("funtranslations", () => {
   it("translates given text with shakespeare translation", async () => {
     const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
 
-    const translation = await service.translateShakespeare("valid text");
+    const translation = await service.translateShakespeare("When it folds");
 
     expect(translation).toBe("Shakespeare translated Garchomp description");
   });
 
-  it("handles bad requests", async () => {
+  it("handles bad requests with yoda translations", async () => {
     const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
 
     await expect(service.translateYoda("")).rejects.toBeInstanceOf(
@@ -26,7 +26,15 @@ describe("funtranslations", () => {
     );
   });
 
-  it("handles api errors", async () => {
+  it("handles bad requests with shakespeare translations", async () => {
+    const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
+
+    await expect(service.translateShakespeare("")).rejects.toBeInstanceOf(
+      TranslationsServiceError
+    );
+  });
+
+  it("handles api errors with yoda translations", async () => {
     const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
 
     await expect(service.translateYoda("error")).rejects.toBeInstanceOf(
@@ -34,11 +42,27 @@ describe("funtranslations", () => {
     );
   });
 
-  it("handles network errors", async () => {
+  it("handles api errors with shakespeare translations", async () => {
+    const service = new FunTranslations(process.env.FUNTRANSLATIONS_URL);
+
+    await expect(service.translateShakespeare("error")).rejects.toBeInstanceOf(
+      TranslationsServiceError
+    );
+  });
+
+  it("handles network errors with yoda translations", async () => {
     const service = new FunTranslations("http://invalid_host");
 
     await expect(service.translateYoda("valid text")).rejects.toBeInstanceOf(
       TranslationsServiceError
     );
+  }, 60_000);
+
+  it("handles network errors with shakespeare translations", async () => {
+    const service = new FunTranslations("http://invalid_host");
+
+    await expect(
+      service.translateShakespeare("valid text")
+    ).rejects.toBeInstanceOf(TranslationsServiceError);
   }, 60_000);
 });
